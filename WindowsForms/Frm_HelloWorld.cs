4b000851f3313a26babeb8b430625a6e95947cb0
Douglas Leal <douglas.leal@live.com>
2023-03-02 18:53:38 -0300

    Label - utilizando o primeiro componente

--- WindowsForms/Frm_HelloWorld.cs
+++ WindowsForms/Frm_HelloWorld.cs
@@ -1,8 +1,8 @@
 namespace WindowsForms
 {
-    public partial class Form1 : Form
+    public partial class Frm_HelloWorld : Form
     {
-        public Form1()
+        public Frm_HelloWorld()
         {
             InitializeComponent();
         }
