7118cb6785452b9d59f4ed650f53c5633ea3d6fb
Douglas Leal <douglas.leal@live.com>
2023-03-02 19:12:34 -0300

    Criando botão para fechar a aplicação

--- WindowsForms/Frm_HelloWorld.Designer.cs
+++ WindowsForms/Frm_HelloWorld.Designer.cs
@@ -3,12 +3,12 @@
     partial class Frm_HelloWorld
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,51 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            Lbl_Titulo = new Label();
+            Btn_Sair = new Button();
+            SuspendLayout();
+            // 
+            // Lbl_Titulo
+            // 
+            Lbl_Titulo.AutoSize = true;
+            Lbl_Titulo.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            Lbl_Titulo.Location = new Point(12, 9);
+            Lbl_Titulo.Name = "Lbl_Titulo";
+            Lbl_Titulo.Size = new Size(308, 40);
+            Lbl_Titulo.TabIndex = 0;
+            Lbl_Titulo.Text = ".NET Windows Forms";
+            // 
+            // Btn_Sair
+            // 
+            Btn_Sair.Location = new Point(713, 415);
+            Btn_Sair.Name = "Btn_Sair";
+            Btn_Sair.Size = new Size(75, 23);
+            Btn_Sair.TabIndex = 1;
+            Btn_Sair.Text = "Sair";
+            Btn_Sair.UseVisualStyleBackColor = true;
+            Btn_Sair.Click += Btn_Sair_Click;
+            // 
+            // Frm_HelloWorld
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(Btn_Sair);
+            Controls.Add(Lbl_Titulo);
+            Name = "Frm_HelloWorld";
+            Text = "Hello World";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Lbl_Titulo;
+        private Button Btn_Sair;
     }
 }
--- WindowsForms/Frm_HelloWorld.cs
+++ WindowsForms/Frm_HelloWorld.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace WindowsForms
 {
     public partial class Frm_HelloWorld : Form
@@ -5,6 +15,11 @@ namespace WindowsForms
         public Frm_HelloWorld()
         {
             InitializeComponent();
+        }
+
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
